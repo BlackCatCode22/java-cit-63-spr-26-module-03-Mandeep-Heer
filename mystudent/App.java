39a5e8bd60bceb7d594717e744c65470c36e3a12
Mandeep-Heer <msheer5567@gmail.com>
2026-02-22 13:33:18 -0800

    Add files via upload

---
+++ mystudent/App.java
@@ -0,0 +1,28 @@
+package mystudent;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String [] args) {
+
+        Student myStudent = new Student();
+        myStudent.firstName = "Cooper";
+        myStudent.lastName = "Howard";
+        myStudent.major = "Business";
+        myStudent.gpa = 2.3;
+        myStudent.age = 24;
+        myStudent.onProbation = false;
+
+        Student myStudent2 = new Student();
+        myStudent.firstName = "Pam";
+        myStudent.lastName = "Beasley";
+        myStudent.major = "Art";
+        myStudent.gpa = 2.5;
+        myStudent.age = 23;
+        myStudent.onProbation = true;
+
+
+
+        System.out.println(myStudent.firstName);
+        System.out.println(myStudent2.firstName);
+    }
+}
